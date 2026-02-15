ccb25586a64d5c6a2a690ce1de3f15896fc21f02
David Herring (DLH) <dherring@salesforce.com>
2026-02-15 18:30:31 -0500

    Trying out Dialogue Manager

---
+++ scenes/arms_area_3d.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+@export var dialogue: DialogueResource
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input_event(camera: Camera3D, event: InputEvent, position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		DialogueManager.show_dialogue_balloon(dialogue, "start")
+		print("Dialogue started")

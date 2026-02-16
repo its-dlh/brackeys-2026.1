8eb9150aadafc6b2546b00b5a54bbe5155ecb21a
David Herring (DLH) <dherring@salesforce.com>
2026-02-15 21:28:48 -0500

    Mouse-based movement

---
+++ scenes/floor_collision.gd
@@ -0,0 +1,9 @@
+extends CollisionObject3D
+
+@onready var player = get_node("/root/MovementTestSceen/Player")
+
+func _input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("Floor click at: ", event_position)
+		print("Player position: ", player.global_position)
+		player.navigate_to(event_position)
--- scenes/player.gd
+++ scenes/player.gd
@@ -2,35 +2,40 @@ extends CharacterBody3D
 
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var camera_marker: Marker3D = $CameraMarker
+@onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 func _ready() -> void:
 	camera_3d.global_position = camera_marker.global_position
 
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 0.5
+	navigation_agent.target_desired_distance = 0.5
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if navigation_agent.is_navigation_finished():
+		return
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	var current_agent_position: Vector3 = global_position
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	velocity = current_agent_position.direction_to(next_path_position) * SPEED
 	move_and_slide()
+
 	camera_3d.global_position = lerp(
 		camera_3d.global_position,
 		camera_marker.global_position,
 		delta * 2.5
 	)
+
+# func _input(event):
+# 	# Mouse in viewport coordinates.
+# 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+# 		print("Mouse click at: ", event.position)
+
+
+func navigate_to(target_position: Vector3) -> void:
+	navigation_agent.set_target_position(target_position)

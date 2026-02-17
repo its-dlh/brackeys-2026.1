aff5e20c1dfaf893ac1dbd7e129d7ff7162806a9
David Herring (DLH) <dherring@salesforce.com>
2026-02-17 11:37:37 -0500

    Allow flexible room state values

--- utils/room_state.gd
+++ utils/room_state.gd
@@ -1,6 +1,6 @@
 extends Node
 
-signal state_changed(key: String, value: bool, room_state: Dictionary)
+signal state_changed(key: String, value, room_state: Dictionary)
 
 var state: Dictionary = {}
 
@@ -10,13 +10,13 @@ func get_room_state() -> Dictionary:
     state[scene_path] = {}
   return state[scene_path]
 
-func set_value(key: String, value: bool) -> void:
+func set_value(key: String, value) -> void:
   var room_state = get_room_state()
   room_state[key] = value
   state_changed.emit(key, value, room_state)
   print('Room state: ', room_state)
 
-func get_value(key: String) -> bool:
+func get_value(key: String):
   var room_state = get_room_state()
-  return room_state.get(key, false)
+  return room_state.get(key, null)
 

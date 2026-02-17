0fd52f9714dfe260a693541c265cbf809bac5bf4
David Herring (DLH) <dherring@salesforce.com>
2026-02-17 10:54:28 -0500

    Make it so that we can use one floor StaticBody3D for both mouse detection and navigation

--- scenes/floor_collision.gd
+++ scenes/floor_collision.gd
@@ -1,6 +1,6 @@
 extends CollisionObject3D
 
-@onready var player = self.get_parent().get_node("Player")
+@onready var player = get_tree().get_first_node_in_group("player")
 
 func _input_event(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:

905d73bbfef996e8b277e62edb7cd5eac29d960d
David Herring (DLH) <dherring@salesforce.com>
2026-02-18 19:29:21 -0500

    Simplifying EditorOnlyGroupVisibility

--- utils/editor_only_group_visibility.gd
+++ utils/editor_only_group_visibility.gd
@@ -1,9 +1,7 @@
-@tool
 extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if not Engine.is_editor_hint():
-		# Hide all editor_only nodes if we're in the game
-		for node in get_tree().get_nodes_in_group("editor_only"):
-			node.visible = false
+	# Hide all editor_only nodes if we're in the game
+	for node in get_tree().get_nodes_in_group("editor_only"):
+		node.visible = false

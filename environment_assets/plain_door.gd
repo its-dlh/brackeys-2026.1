1ce51ace2f9fd5757a2828bbe22fa2278a23996a
David Herring (DLH) <dherring@salesforce.com>
2026-02-20 10:39:01 -0500

    Little hallway previews for doors

--- environment_assets/plain_door.gd
+++ environment_assets/plain_door.gd
@@ -2,22 +2,25 @@ extends Node3D
 
 @export var dialogue: DialogueResource
 @export var open_state_key: String = "door_open"
+@export var start_open: bool = false
 @export var nodes_to_reveal: Array[Node3D] = []
+@export var nodes_to_hide: Array[Node3D] = []
 
 @onready var interactable = $Interactable
 @onready var door_mesh: MeshInstance3D = $MeshInstance3D
 @onready var door_collision_shape: CollisionShape3D = $StaticBody3D/CollisionShape3D
 
+var has_been_open: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	RoomState.state_changed.connect(_on_state_changed)
-
-	if not is_door_open():
-		for node in nodes_to_reveal:
-			node.visible = false
+	if start_open:
+		open_door()
 
 	# Reflect the initial state of the door
 	_on_open_state_changed(is_door_open())
+	# Reflect future state changes
+	RoomState.state_changed.connect(_on_state_changed)
 
 	if dialogue:
 		interactable.dialogue = dialogue
@@ -31,12 +34,21 @@ func _on_open_state_changed(is_open: bool) -> void:
 		print('Reflecting door state: open')
 		door_mesh.position.x = 1.4
 		door_collision_shape.set_deferred("disabled", true)
-		for node in nodes_to_reveal:
-			node.visible = true
+
+		if not has_been_open:
+			has_been_open = true
+			for node in nodes_to_reveal:
+				node.visible = true
+			for node in nodes_to_hide:
+				node.visible = false
 	else:
 		print('Reflecting door state: closed')
 		door_mesh.position.x = 0.0
 		door_collision_shape.set_deferred("disabled", false)
+
+		if not has_been_open:
+			for node in nodes_to_reveal:
+				node.visible = false
 
 func open_door() -> void:
 	RoomState.set_value(open_state_key, true)

e88f6177f26d2859b617dae9fe40698dd244ef21
Dawson Goodell <dawsong@osmstudios.com>
2026-02-20 00:06:50 -0800

    player character model and rotation to follow our direction of movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,9 +3,11 @@ extends CharacterBody3D
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var camera_marker: Marker3D = $CameraMarker
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var mesh: Node3D = $"player-character-v2"
 
 const CAMERA_LAG_FACTOR = 2.5
 const SPEED = 5.0
+const ROTATION_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
 func _ready() -> void:
@@ -19,6 +21,10 @@ func _physics_process(delta: float) -> void:
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 
 	velocity = current_agent_position.direction_to(next_path_position) * SPEED
+	
+	# Look where we're going
+	var target_angle = atan2(-velocity.x, -velocity.z) + PI # rotate 180 degrees cuz reasons
+	mesh.rotation.y = lerp_angle(mesh.rotation.y, target_angle, ROTATION_SPEED * delta)
 
 	# TODO override navigation if controls are used
 	# var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")

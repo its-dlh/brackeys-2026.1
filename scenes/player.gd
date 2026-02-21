4fc64649d3377c09f866f0fdebeec545168af270
David Herring (DLH) <dherring@salesforce.com>
2026-02-21 18:11:08 -0500

    First pass at footsteps

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @onready var camera_marker: Marker3D = $CameraMarker
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var mesh: Node3D = $"player-character-v2"
+@onready var rusty_player = $RustyPlayer
 
 const CAMERA_LAG_FACTOR = 2.5
 const SPEED = 5.0
@@ -15,6 +16,8 @@ func _ready() -> void:
 	mesh.get_node("RightArm").visible = GameState.get_value(GameState.HAS_RIGHT_ARM) or false
 	GameState.state_changed.connect(_game_state_change)
 	camera_3d.global_position = camera_marker.global_position
+	navigation_agent.path_changed.connect(_on_path_changed)
+	navigation_agent.navigation_finished.connect(_on_navigation_finished)
 
 func _physics_process(delta: float) -> void:
 	if navigation_agent.is_navigation_finished():
@@ -24,7 +27,7 @@ func _physics_process(delta: float) -> void:
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 
 	velocity = current_agent_position.direction_to(next_path_position) * SPEED
-	
+
 	# Look where we're going
 	var target_angle = atan2(-velocity.x, -velocity.z) + PI # rotate 180 degrees cuz reasons
 	mesh.rotation.y = lerp_angle(mesh.rotation.y, target_angle, ROTATION_SPEED * delta)
@@ -59,3 +62,11 @@ func _game_state_change(key, value, old_state, new_state):
 func navigate_to(target_position: Vector3, desired_distance: float = 0.1) -> void:
 	navigation_agent.set_target_position(target_position)
 	navigation_agent.target_desired_distance = desired_distance
+
+func _on_path_changed() -> void:
+	if not rusty_player.playing:
+		rusty_player.play(0.0)
+
+func _on_navigation_finished() -> void:
+	if rusty_player.playing:
+		rusty_player.stop()

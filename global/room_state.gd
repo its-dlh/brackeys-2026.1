92ab489894c0cbd13849db2342a380bb3c994d61
Dawson Goodell <dawsong@osmstudios.com>
2026-02-21 11:27:01 -0800

    computer room

--- global/room_state.gd
+++ global/room_state.gd
@@ -5,19 +5,18 @@ signal state_changed(key: String, value, prev_value, room_state: Dictionary)
 var state: Dictionary = {}
 
 func get_room_state() -> Dictionary:
-  var scene_path = get_tree().current_scene.scene_file_path
-  if not state.has(scene_path):
-    state[scene_path] = {}
-  return state[scene_path]
+	var scene_path = get_tree().current_scene.scene_file_path
+	if not state.has(scene_path):
+		state[scene_path] = {}
+	return state[scene_path]
 
 func set_value(key: String, value) -> void:
-  var room_state = get_room_state()
-  var prev_value = room_state.get(key, null)
-  room_state[key] = value
-  state_changed.emit(key, value, prev_value, room_state)
-  print('Room state: ', room_state)
+	var room_state = get_room_state()
+	var prev_value = room_state.get(key, null)
+	room_state[key] = value
+	state_changed.emit(key, value, prev_value, room_state)
+	print('Room state: ', room_state)
 
 func get_value(key: String):
-  var room_state = get_room_state()
-  return room_state.get(key, null)
-
+	var room_state = get_room_state()
+	return room_state.get(key, null)

b031f135b6e3157e0010bad2474b5b632bfcbec6
Dawson Goodell <dawsong@osmstudios.com>
2026-02-21 19:06:25 -0800

    dead bodies, ending scene

--- scenes/outside.gd
+++ scenes/outside.gd
@@ -1,7 +1,8 @@
 extends Node3D
 
-
+@onready var resource: DialogueResource = load("res://words/end-game.dialogue")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	DialogueManager.show_dialogue_balloon(resource)
 	if MusicPlayer.playing:
 		MusicPlayer.stop()

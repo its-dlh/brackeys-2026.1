3a04d5937136c8b0948c74f39bccc46fb895d3fa
David Herring (DLH) <dherring@salesforce.com>
2026-02-21 20:02:02 -0500

    Sound effects on getting arm

--- global/sound_effects.gd
+++ global/sound_effects.gd
@@ -2,9 +2,13 @@ extends Node
 
 @onready var door_player = $DoorPlayer
 @onready var squeaky_locker_player = $SqueakyLockerPlayer
+@onready var take_item_player = $TakeItemPlayer
 
 func play_door_sound() -> void:
 	door_player.play()
 
 func play_squeaky_locker_sound() -> void:
 	squeaky_locker_player.play()
+
+func play_take_arm_sound() -> void:
+	take_item_player.play()

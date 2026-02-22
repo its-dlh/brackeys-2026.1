24f4cafd8e3f5cb6cf1bbfd5a1fe2ac818b11a5f
David Herring (DLH) <dherring@salesforce.com>
2026-02-21 21:02:17 -0500

    Lockdown sounds

--- global/sound_effects.gd
+++ global/sound_effects.gd
@@ -3,6 +3,8 @@ extends Node
 @onready var door_player = $DoorPlayer
 @onready var squeaky_locker_player = $SqueakyLockerPlayer
 @onready var take_item_player = $TakeItemPlayer
+@onready var lockdown_disabled = $LockdownDisabled
+@onready var lockdown_enabled = $LockdownEnabled
 
 func play_door_sound() -> void:
 	door_player.play()
@@ -12,3 +14,9 @@ func play_squeaky_locker_sound() -> void:
 
 func play_take_arm_sound() -> void:
 	take_item_player.play()
+
+func play_lockdown_disabled_sound() -> void:
+	lockdown_disabled.play()
+
+func play_lockdown_enabled_sound() -> void:
+	lockdown_enabled.play()
